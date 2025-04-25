405b507962f9786f0c76a6f374702746e6c49101
Moriartti <thisisjhenna@gmail.com>
2025-04-25 21:54:00 +0800

    Update navigation path in Login component to redirect to admin page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -22,7 +22,7 @@ export default function Login() {
 
     useEffect(() => {
         if (isAuthenticated()) {
-            navigate('/card');
+            navigate('/admin'); // navigate('card');
         }
     }, [isAuthenticated, navigate]);
 
